73396a93a37fbd28c85e5f8d6263c79a58898f90
Tetiana <bta1985@ukr.net>
2022-06-18 23:29:15 +0300

    add AboutPage

--- src/pages/About/AboutPage.js
+++ src/pages/About/AboutPage.js
@@ -1,7 +1,48 @@
-import React from "react";
-
+import React from 'react'
+import './AboutPage.scss'
 export const AboutPage = () => {
     return (
-        <h1>About</h1>
+        <>
+            <section className='aboutPage'>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12 companyDesc">
+                            <h2 className="h2 name">Our company</h2>
+                        </div>
+
+                        <div className="col-md-12 companyDesc">
+                            <h2 className="h2">Vestibulum tincidunt</h2>
+                            <p>Aenean augue eu quam. Maecenas pretium, ipsum ullamcorper ac, felis. Pellentesque habitant morbi tristique mauris vehicula sit amet erat. Sed et luctus augue imperdiet tincidunt</p>
+                        </div>
+                        <div className="col-md-6">
+                            <h3 className="h3">Curabitur tempor</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet quam. Phasellus a
+                                ipsum. Fusce imperdiet quis, eleifend justo at
+                                quam. Curabitur tempor. Phasellus fermentum
+                                nonummy consequat ligula in sollicitudin digni
+                                ssim, nulla sed est et magnis dis parturient
+                                montes, nascetur ridiculus mus. Integer magna
+                                ipsum sit amet erat. Sed a leo semper egestas.
+                                Cum sociis natoque penatibus et magnis.
+                            </p>
+                        </div>
+                        <div className="col-md-6">
+                            <h3 className="h3">Duis ipsum</h3>
+                            <p>
+                                Lorem ipsum dolor sit amet quam. Phasellus a
+                                ipsum. Fusce imperdiet quis, eleifend justo at
+                                quam. Curabitur tempor. Phasellus fermentum
+                                nonummy consequat ligula in sollicitudin digni
+                                ssim, nulla sed est et magnis dis parturient
+                                montes, nascetur ridiculus mus. Integer magna
+                                ipsum sit amet erat. Sed a leo semper egestas.
+                                Cum sociis natoque penatibus et magnis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
     )
 }
