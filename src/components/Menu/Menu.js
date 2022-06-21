cfeeb4c7b0b0c1df4b18ba0ac0a30f4ca5bf337c
Tetiana <bta1985@ukr.net>
2022-06-21 15:51:09 +0300

    add corections in styles

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Link, NavLink } from 'react-router-dom'
 import './Menu.scss'
-import { DropdownButton, Dropdown } from 'react-bootstrap'
 
 export const Menu = () => {
     return (
